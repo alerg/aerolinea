60db6d35f0f0e7cac29c949ce10dec90e3baae3b
alerg <alejandroraulgarcia@gmail.com>
2014-07-22 02:15:30 -0300

    Graficos
    Graficos en PDF de admin. Ojo que algunos rompen.

--- template/grafico.php
+++ template/grafico.php
@@ -1,7 +1,7 @@
 <?php
 // agregamos la bibliotecas necesarias
-require_once ('../lib/jpgraph/src/jpgraph.php');
-require_once ('../lib/jpgraph/src/jpgraph_pie.php');
+require_once('../lib/jpgraph/jpgraph.php');
+require_once('../lib/jpgraph/jpgraph_pie.php');
 
 include "../core/ConexionMySQL.php";
 
@@ -13,58 +13,67 @@ if($id == '1' || $id == '2' || $id == '3' || $id == '4'){
 	switch($id){
 		case '1'://Cantidad de pasajes vendidos
 			$titulo = 'Cantidad de pasajes vendidos';
-			$query = 'SELECT COUNT(*) as cant_pasaje_vendidos FROM pasaje;';
+			$label = array();
+			$query = 'SELECT COUNT(*) as Pasajes FROM pasaje;';
 		break;
 		case '2'://Cantidad de pasajes vendidos por categoría:
 			$titulo = 'Cantidad de pasajes vendidos por categoría';
-			$query = 'select categoria, count(*) as cantidad '.
+			$label = array('Categoria');
+			$query = 'select Categoria, count(*) as Cantidad '.
 					'from pasaje  '.
 					'group by categoria; ';
 		break;
 		case '3'://Cantidad de pasajes vendidos  por destino:
 			$titulo = 'Cantidad de pasajes vendidos  por destino';
-			$query = 'select a.ciudad, count(*) as cantidad '.
+			$label = array('Ciudad destino');
+			$query = 'select a.ciudad as \'Ciudad destino\', count(*) as Cantidad '.
 					'from pasaje as p join vuelo as v on p.id_vuelo=v.id_vuelo join recorrido as r on v.id_recorrido=r.id_recorrido join aeropuerto as a on a.ciudad=r.id_ciudad_destino '.
 					'group by a.ciudad; ';
 		break;
 		case '4'://Cantidad de reservas caídas:
 			$titulo = 'Cantidad de reservas caídas';
-			$query = 'select count(*) '.
+			$label = array('Reservas caídas');
+			$query = 'select count(*) as \'Reservas caídas\' '.
 					'from pasaje as p join vuelo as v on p.id_vuelo=v.id_vuelo '.
 					'where p.id_estado=0 and v.fecha<DATE(NOW()); ';
 		break;
 	}
-}
-
 
 	$conexion = new ConexionMySQL();
 	$matriz = $conexion->ejecutarQuery($query);
 	$valores = array();
-		for ($i=0; $i < count($matriz); $i++) {
-			$registro = $matriz[$i];
-		 	foreach ($registro as $key => $value) {
-			echo $value;
-		 		array_push($valores, utf8_encode($value));
-			}
+	$labels = array();
+	for ($i=0; $i < count($matriz); $i++) {
+		$registro = $matriz[$i];
+	 	foreach ($registro as $key => $value) {
+	 		if(! in_array($key, $label)){
+	 			if(intval($value) > 0)
+		 			array_push($valores, intval($value));
+		 	}else{
+		 		array_push($labels, $value .' - %.1f%%');
+		 	}
+
 		}
+	}
+	if(count($valores) > 0){
+		$ancho = 600; $alto = 250;
+		//crear la instancia del objeto graph
+		$graph = new PieGraph($ancho,$alto, 'auto');
+		//especificar la escala
+		$graph-> SetScale('intint');
+		//crear curva
+		$curva = new PiePlot($valores);
+
+		$curva->SetLabelType(PIE_VALUE_PER); 
+		$curva->SetLabels($labels);
+
+		// Configuraciones del grafico
+		// agregar la curva al grafico
+		$graph->Add($curva);
+		// mostrar el grafico
+		$graph->Stroke();
+	}
+}
 
 
-// agregamos la bibliotecas necesarias
-require_once ('../lib/jpgraph/src/jpgraph.php');
-require_once ('../lib/jpgraph/src/jpgraph_pie.php');
-//definimos los datos
-$datos = array(5,8,5,6,5,2,25,8,10);
-// Definir ancho y alto del grafico
-$ancho = 600; $alto = 250;
-//crear la instancia del objeto graph
-$graph = new PieGraph($ancho,$alto, 'auto');
-//especificar la escala
-$graph-> SetScale('intint');
-//crear curva
-$curva = new PiePlot($valores);
-// Configuraciones del grafico
-// agregar la curva al grafico
-$graph->Add($curva);
-// mostrar el grafico
-$graph->Stroke();
 ?>
